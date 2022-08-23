f96fd9385c8eb8f06aed83c173075d32b71bb5f2
nmaresco <109701914+nmaresco@users.noreply.github.com>
2022-08-23 14:17:48 -0700

    JS Bug Fixes and adding a Loop to Print

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,9 @@
 let pokemonList = [
-  {name:'Pikachu',height:4,type:'electric'}
-  {name:'Charmander',height:6,type:'fire'}
+  {name:'Pikachu',height:4,type:'electric'},
+  {name:'Charmander',height:6,type:'fire'},
   {name:'Squirtle', height:5,type:'water'}
 ];
+for (let i = 1; i <=1;i++){
+      document.write(pokemonList),
+      console.log(pokemonList);
+}
